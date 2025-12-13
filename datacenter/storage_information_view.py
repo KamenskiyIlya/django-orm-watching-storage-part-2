5a389b6b5f064f1ac0ac0a134e982686dc043ba9
KamenskiyIlya <kamenskj.ilya@gmail.com>
2025-12-13 20:54:38 +0300

    Убрал лишние переменные из storage_information_view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,15 +10,11 @@ def storage_information_view(request):
     not_leaved_visiters = Visit.objects.filter(leaved_at__isnull=True)
 
     for visit in not_leaved_visiters:
-        visiter_passcard = visit.passcard.owner_name
-        entered_time = visit.entered_at
-        stay_time = visit.get_duration()
-        stay_time = visit.format_duration(stay_time)
 
         visit_content = {
-            'who_entered': visiter_passcard,
-            'entered_at': entered_time,
-            'duration': stay_time,
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': visit.format_duration(visit.get_duration()),
         }
 
         non_closed_visits.append(visit_content)
